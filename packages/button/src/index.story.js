e95016e5ca55f3a1bd70cf6d42070a2afb204d7d
Kristi Jorgji <=>
2020-04-02 14:35:05 +0200

    try to import new design

--- packages/button/src/index.story.js
+++ packages/button/src/index.story.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {storiesOf} from '@storybook/react';
-
+import { Card } from '@flaconi/dummy-library/build/components';
 import Button from '.';
 
-storiesOf('Button', module).add('default', () => <Button>{'Button'}</Button>);
+storiesOf('Button', module).add('default', () => <Fragment>>
+    <Card/>
+    <Button>{'Button'}</Button>
+</Fragment>);
