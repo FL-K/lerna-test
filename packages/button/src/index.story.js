3bd9a92a4a45275bba75826115001fb8381f5512
Kristi Jorgji <=>
2020-04-03 15:24:20 +0200

    testing the publsihed design library

--- packages/button/src/index.story.js
+++ packages/button/src/index.story.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {storiesOf} from '@storybook/react';
+import { AlertBox } from '@flaconi/dandy-components-library/build/components';
+import {  Input } from '@flaconi/dandy-components-library/build/components';
+import Light from '@flaconi/dandy-components-library/build/styles/themes/light/Light';
+console.log(Light);
 
 import Button from '.';
+import {ThemeProvider} from "styled-components";
 
-storiesOf('Button', module).add('default', () => <Button>{'Button'}</Button>);
+storiesOf('Button', module).add('default', () =>
+    <ThemeProvider theme={Light}>
+        <Fragment>
+            <AlertBox/>
+            <Input placeholder="You can edit me" />
+            <Button>{'Button'}</Button>
+        </Fragment>
+    </ThemeProvider>
+);
